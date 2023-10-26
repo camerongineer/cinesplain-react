8cac4e32ef7a501bc8364f09f4a6b1f6d09c30f5
camerongineer <alphaomegabeta@gmail.com>
2023-10-26 12:36:50 -0700

    Add option to add label to front of release date

--- src/ui/components/common/ReleaseDateDisplay.tsx
+++ src/ui/components/common/ReleaseDateDisplay.tsx
@@ -6,15 +6,17 @@ import { getFormattedDate } from "../../../utils/formatUtils";
 
 interface ReleaseDateDisplayProps {
     releaseDate: string;
+    includeLabel?: boolean;
     sx?: SxProps<Theme>;
 }
 
-const ReleaseDateDisplay: React.FC<ReleaseDateDisplayProps> = ({ releaseDate, sx }) => {
+const ReleaseDateDisplay: React.FC<ReleaseDateDisplayProps> = ({ releaseDate, includeLabel, sx }) => {
+    const releaseLabelText = new Date() > new Date(releaseDate) ? "Released on" : "Releases on"
     return (
         <>
             <StandardTypography
                 variant={"body1"}
-                sx={sx}>{getFormattedDate(releaseDate)}
+                sx={sx}>{(includeLabel ? <b>{releaseLabelText}:&nbsp;</b> : "")}{getFormattedDate(releaseDate)}
             </StandardTypography>
         </>
     );
