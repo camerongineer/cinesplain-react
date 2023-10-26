f3cc9454337e7f1f8d97b850170f7491a8c7ddd0
camerongineer <alphaomegabeta@gmail.com>
2023-10-26 13:01:24 -0700

    Add getFormattedRuntime

--- src/utils/formatUtils.ts
+++ src/utils/formatUtils.ts
@@ -12,3 +12,9 @@ export const getFormattedDate = (dateString: string | undefined) => {
 export const getFormattedCurrencyAmount = (dollarAmount: number) => {
     return dollarAmount.toLocaleString("en-us", { style: "currency", currency: "USD", minimumFractionDigits: 0 });
 };
+
+export const getFormattedRuntime = (runtimeMinutes: number) => {
+    const hours = Math.floor(runtimeMinutes / 60);
+    const minutes = runtimeMinutes % 60;
+    return `${hours > 0 ? `${hours}h `: ""}${minutes}m`
+}
