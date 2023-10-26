eee2217237709945ee1c17dc76b9d80ef2b827f4
camerongineer <alphaomegabeta@gmail.com>
2023-10-26 15:21:10 -0700

    Add RuntimeDisplay

---
+++ src/ui/components/common/RuntimeDisplay.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { SxProps } from "@mui/system";
+import { Theme } from "@mui/material/styles";
+import { getFormattedRuntime } from "../../../utils/formatUtils";
+import { Typography } from "@mui/material";
+
+interface RuntimeDisplayProps {
+    runtime: number;
+    includeLabel?: boolean;
+    sx?: SxProps<Theme>;
+}
+
+const RuntimeDisplay: React.FC<RuntimeDisplayProps> = ({ runtime, includeLabel, sx }) => (
+    <>
+        <Typography sx={sx}
+                    variant={"body1"}>
+            {includeLabel && <b>Runtime:&nbsp;</b>}
+            {runtime > 0 ? getFormattedRuntime(runtime) : "Unknown"}
+        </Typography>
+    </>
+);
+
+export default RuntimeDisplay;
