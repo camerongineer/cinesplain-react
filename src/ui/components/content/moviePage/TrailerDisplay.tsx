49be3fdf15caf7e353d2e530dfe348cebdea8907
camerongineer <alphaomegabeta@gmail.com>
2023-10-26 15:31:42 -0700

    Add Paper container

--- src/ui/components/content/moviePage/TrailerDisplay.tsx
+++ src/ui/components/content/moviePage/TrailerDisplay.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Paper, styled } from "@mui/material";
 import { SxProps } from "@mui/system";
 import { Theme } from "@mui/material/styles";
 import Movie from "../../../../models/movie";
 import { getYouTubeTrailerPath } from "../../../../utils/retrievalUtils";
+
+const StyledPaper = styled(Paper)`
+  background: linear-gradient(270deg, ${props => props.theme.palette.grey[200]}, ${props => props.theme.palette.grey[500]});
+  padding: 10px 10px 0 10px;
+  width: 100%;
+`;
 
 interface TrailerCardProps {
     movie: Movie;
@@ -13,7 +19,7 @@ interface TrailerCardProps {
 const TrailerDisplay: React.FC<TrailerCardProps> = ({ movie, sx }) => {
     const trailers = movie.videos.filter(video => video.videoType === "Trailer");
     return (
-        <>
+        <StyledPaper sx={sx} elevation={5}>
             {movie && trailers.length > 0 && <Box sx={sx}
                                                   component={"iframe"}
                                                   src={getYouTubeTrailerPath(trailers[0].videoKey)}
@@ -21,7 +27,7 @@ const TrailerDisplay: React.FC<TrailerCardProps> = ({ movie, sx }) => {
                                                   allow=""
                                                   border={"0"}
                                                   allowFullScreen></Box>}
-        </>
+        </StyledPaper>
     );
 };
 
