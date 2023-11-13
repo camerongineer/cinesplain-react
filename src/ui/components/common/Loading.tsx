b1f84c1c361c7476de7995358834d45211c162e5
Cameron Marr <alphaomegabeta@gmail.com>
2023-11-13 13:33:50 -0800

    Add Loading screen

---
+++ src/ui/components/common/Loading.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Stack, styled } from "@mui/material";
+
+const StyledStack = styled(Stack)`
+  position: relative;
+  height: 94vh;
+  overflow: hidden;
+`;
+
+const StyledIcon = styled("img")`
+  width: 25%;
+  animation: spin 2000ms linear infinite;
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const StyledLogo = styled("img")`
+  width: 40%;
+  padding-top: 20px;
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const Loading = () => {
+    return (
+        <StyledStack className="full center">
+            <StyledIcon src={require("../../images/cs_logo_up.png")} alt={"spinning icon"}/>
+            <StyledLogo src={require("../../images/cs_logo_text.png")} alt={"CineSplain logo"}/>
+        </StyledStack>
+    );
+};
+
+export default Loading;
