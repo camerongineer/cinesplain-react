317bd9b5427af23c9b67f0aa56eb840e6298fa0b
Cameron Marr <alphaomegabeta@gmail.com>
2023-12-19 11:39:14 -0800

    Modify colors and size

--- src/ui/components/common/PopcornRating.tsx
+++ src/ui/components/common/PopcornRating.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import popcornRating from "../../images/popcorn_rating.png";
 import { Box, styled } from "@mui/material";
+import { grey } from "@mui/material/colors";
 
 const RatingCircle = styled(Box)`
-  display: flex;
-  position: absolute;
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  border: 2px solid black;
-  left: 25px;
-  bottom: 17px;
+    display: flex;
+    position: absolute;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    border: 2px solid black;
+    left: 25px;
+    bottom: 15px;
+    color: ${grey[100]};
+    font-weight: bolder;
 `;
 
 interface CSRatingProps {
@@ -22,8 +25,8 @@ const PopcornRating: React.FC<CSRatingProps> = ({
 }) => {
     const displayedRating = Math.floor(voteAverage * 10);
     const ratingColor = displayedRating < 60 ?
-        "rgba(81,75,20,0.7)" : displayedRating < 70 ?
-            "rgba(59,118,31,0.7)" : "rgba(31,148,61,0.7)";
+        "rgba(236,122,39,0.7)" : displayedRating < 80 ?
+            "rgba(255,132,232,0.7)" : "rgba(76,67,212,0.7)";
     
     return (
         <Box position={"relative"} width={"fit-content"}>
@@ -32,7 +35,11 @@ const PopcornRating: React.FC<CSRatingProps> = ({
                  height={"40px"}
                  src={popcornRating}
                  alt={displayedRating.toString()}/>
-            <RatingCircle className={"center"} bgcolor={ratingColor}>{displayedRating}</RatingCircle>
+            <RatingCircle
+                className={"center"}
+                bgcolor={ratingColor}>
+                {displayedRating}
+            </RatingCircle>
         </Box>
     );
 };
