04764fe4da1d07a6a63c87dccf6e95c1c833d534
Cameron Marr <alphaomegabeta@gmail.com>
2023-12-23 10:34:46 -0800

    Remove StandardTypography; Refactor line breaks

--- src/ui/components/content/search/MovieSearchRow.tsx
+++ src/ui/components/content/search/MovieSearchRow.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { Stack, styled } from "@mui/material";
+import { Stack, styled, Typography } from "@mui/material";
 import OuterCarousel from "../../common/OuterCarousel";
 import Movie from "../../../../models/movie";
 import { Link } from "react-router-dom";
 import MovieCard from "../moviePage/MovieCard";
-import { StandardTypography } from "../../../styles/Typography";
 
 const StyledStack = styled(Stack)`
-  flex-direction: row;
-  align-items: center;
-  min-width: 100%;
-  justify-content: start;
+    flex-direction: row;
+    align-items: center;
+    min-width: 100%;
+    justify-content: start;
 `;
 
 interface MovieSearchRowProps {
@@ -19,29 +18,48 @@ interface MovieSearchRowProps {
     invalidQueryPrompt: boolean;
 }
 
-const MovieSearchRow: React.FC<MovieSearchRowProps> = ({ movies, onModalEvent, invalidQueryPrompt }) => {
+const MovieSearchRow: React.FC<MovieSearchRowProps> = ({
+    movies,
+    onModalEvent,
+    invalidQueryPrompt
+}) => {
     return (
         <>
-            {movies.length > 0 && <OuterCarousel sx={{ overflow: "auto", height: "inherit" }}>
-                <StyledStack className={"full center"}>
+            {movies.length > 0 && <OuterCarousel sx={{
+                overflow: "auto",
+                height: "inherit"
+            }}>
+                <StyledStack className="full center">
                     {movies.length > 0 && movies.slice(0, 20).map(movie => {
                         return (
-                            <Link key={movie.movieId} to={`/movies/${movie.movieId}`} onClick={onModalEvent}>
-                                <MovieCard movie={movie}
-                                           posterSize={"w342"}
-                                           sx={{
-                                               height: "auto",
-                                               width: { xs: "70vw", sm: "26vw", md: "20vw", lg: "15vw" },
-                                               minWidth: "100px",
-                                               cursor: "pointer",
-                                               margin: "10px"
-                                           }}
-                                           onHover={() => {}}
-                                           isExpandable={true}/>
+                            <Link
+                                key={movie.movieId}
+                                to={`/movies/${movie.movieId}`}
+                                onClick={onModalEvent}>
+                                <MovieCard
+                                    key={movie.movieId}
+                                    movie={movie}
+                                    posterSize="w342"
+                                    sx={{
+                                        height: "auto",
+                                        width: {
+                                            xs: "70vw",
+                                            sm: "26vw",
+                                            md: "20vw",
+                                            lg: "15vw"
+                                        },
+                                        minWidth: "100px",
+                                        cursor: "pointer",
+                                        margin: "10px"
+                                    }}
+                                    onHover={() => {}}
+                                    isExpandable={true}/>
                             </Link>);
                     })}
                     {invalidQueryPrompt && movies.length === 0 &&
-                        <StandardTypography variant={"h2"}>No movie data available.</StandardTypography>}
+                        <Typography variant={"h2"}>
+                            No movie data available.
+                        </Typography>}
                 </StyledStack>
             </OuterCarousel>}
         </>
