d10e700cab42fdaec11d190eb178a2190a638a82
Cameron Marr <alphaomegabeta@gmail.com>
2023-12-23 10:20:31 -0800

    Modify root loader to load headerData and homePageData

--- src/ui/components/Router.tsx
+++ src/ui/components/Router.tsx
@@ -4,13 +4,18 @@ import HomePage, { homePageLoader } from "./content/homePage/HomePage";
 import MoviePage, { moviePageLoader } from "./content/moviePage/MoviePage";
 import Layout from "./Layout";
 import Loading from "./common/Loading";
+import { headerLoader } from "./header/Header";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route
             id="root"
             element={<Layout/>}
-            loader={homePageLoader}>
+            loader={async () => {
+                const headerData = await headerLoader();
+                const homePageData = await homePageLoader();
+                return { headerData, ...homePageData };
+            }}>
             <Route
                 index
                 element={<HomePage/>}
