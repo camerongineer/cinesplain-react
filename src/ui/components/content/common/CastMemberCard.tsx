a6d01880305c733ecf95c890fac0609ea9932eda
Cameron Marr <alphaomegabeta@gmail.com>
2023-12-27 16:11:30 -0800

    Style: Change height and add lazy loading

--- src/ui/components/content/common/CastMemberCard.tsx
+++ src/ui/components/content/common/CastMemberCard.tsx
@@ -1,20 +1,24 @@
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardMedia,
+    styled,
+    Typography
+} from "@mui/material";
+import { SxProps } from "@mui/system";
 import React, { useState } from "react";
-import { Card, CardContent, CardHeader, CardMedia, styled, Typography } from "@mui/material";
+import { PROFILE_SIZE } from "../../../../constants/ImageSizes";
 import CastMember from "../../../../models/castMember";
 import { getImagePath } from "../../../../utils/retrievalUtils";
 import femaleSilhouette from "../../../images/female_silhouette.png";
 import maleSilhouette from "../../../images/male_silhouette.png";
-import { SxProps } from "@mui/system";
-import { PROFILE_SIZE } from "../../../../constants/ImageSizes";
 
 const StyledCard = styled(Card)`
-  overflow: visible;
-  height: fit-content;
-  min-width: 200px;
-  margin: 5px;
-  font-size: calc(10px + 2vmin);
-  color: ${props => props.theme.palette.text.primary};
-  transition: opacity ${props => props.theme.transitions.duration.complex}ms ease-in-out;
+    height: fit-content;
+    min-width: 200px;
+    margin: 5px;
+    transition: opacity ${props => props.theme.transitions.duration.complex}ms ease-in-out;
 `;
 
 interface CastMemberCardProps {
@@ -29,26 +33,33 @@ const CastMemberCard: React.FC<CastMemberCardProps> = ({ castMember, sx }) => {
     const altSilhouette = castMember.gender === 1 ? femaleSilhouette : maleSilhouette;
     
     return (
-        <StyledCard elevation={5}
-                    sx={sx}
-                    style={{ opacity: loading ? "0" : "1" }}
-                    key={castMember.castMemberId}
-                    onMouseEnter={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}>
+        <StyledCard
+            elevation={5}
+            sx={sx}
+            style={{ opacity: loading ? "0" : "1" }}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}>
             {<CardContent sx={{ position: "relative" }}>
-                <Typography variant={"body2"}>{castMember.castMemberName}</Typography>
+                <Typography variant="body2">
+                    {castMember.castMemberName}
+                </Typography>
             </CardContent>}
             <CardMedia
                 component="img"
+                loading="lazy"
                 image={castMember.profilePath
                     ? getImagePath(castMember.profilePath, PROFILE_SIZE.MD_W185)
                     : altSilhouette}
-                height={"200px"}
                 alt={castMember.castMemberName}
+                height={180}
                 onLoad={() => setLoading(false)}
             />
             {castMember.character && <CardHeader
-                title={<Typography variant={"body2"}>"<strong>{castMember.character}</strong>"</Typography>}/>}
+                title={
+                    <Typography
+                        variant="body2">
+                        "<strong>{castMember.character}</strong>"
+                    </Typography>}/>}
         </StyledCard>
     );
 };
