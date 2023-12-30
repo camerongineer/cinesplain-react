14a8112b28e741eb25394527dc9c050298feff18
Cameron Marr <alphaomegabeta@gmail.com>
2023-12-30 13:01:26 -0800

    Style: Add loading effect to background

--- src/ui/components/Layout.tsx
+++ src/ui/components/Layout.tsx
@@ -1,9 +1,16 @@
 import {
     Stack,
-    styled
+    styled,
+    useTheme
 } from "@mui/material";
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, {
+    useEffect,
+    useState
+} from "react";
+import {
+    Outlet,
+    useNavigation
+} from "react-router-dom";
 import useScrollToTop from "../../hooks/UseScrollToTop";
 import Header from "./header/Header";
 
@@ -13,9 +20,35 @@ const StyledStack = styled(Stack)`
 `;
 
 const Layout: React.FC = () => {
+    const [degrees, setDegrees] = useState(100);
+    const theme = useTheme();
+    const navigation = useNavigation();
+    
+    useEffect(() => {
+        if (navigation.state === "loading") {
+            const intervalId = setInterval(() => {
+                setDegrees((prevDegrees) => (prevDegrees + 1) % 360);
+            }, 15);
+            
+            return () => clearInterval(intervalId);
+        }
+    }, [navigation.state]);
+    
     useScrollToTop();
+    const getLoadingStyle = () => {
+        return {
+            background:
+                navigation.state === "loading"
+                    ? `linear-gradient(${degrees}deg,
+                        ${theme.palette.secondary.main}80,
+                        ${theme.palette.secondary.main}25,
+                        ${theme.palette.secondary.main}80),
+                        ${theme.palette.background.default}`
+                    : ""
+        };
+    };
     return (
-        <Stack className="full center">
+        <Stack className="full center" style={getLoadingStyle()}>
             <Header/>
             <StyledStack>
                 <Outlet/>
