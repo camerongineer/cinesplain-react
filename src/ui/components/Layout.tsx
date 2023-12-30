fb99c22de52c17c9a465baf3357054c123977125
Cameron Marr <alphaomegabeta@gmail.com>
2023-12-30 11:01:34 -0800

    Chore: Add UseScrollToTop hook for scrolling to the top of page when navigating to new page

--- src/ui/components/Layout.tsx
+++ src/ui/components/Layout.tsx
@@ -1,6 +1,10 @@
+import {
+    Stack,
+    styled
+} from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Stack, styled } from "@mui/material";
+import useScrollToTop from "../../hooks/UseScrollToTop";
 import Header from "./header/Header";
 
 const StyledStack = styled(Stack)`
@@ -8,13 +12,16 @@ const StyledStack = styled(Stack)`
     width: 100%;
 `;
 
-const Layout: React.FC = () => (
-    <Stack className="full center">
-        <Header/>
-        <StyledStack>
-            <Outlet/>
-        </StyledStack>
-    </Stack>
-);
+const Layout: React.FC = () => {
+    useScrollToTop();
+    return (
+        <Stack className="full center">
+            <Header/>
+            <StyledStack>
+                <Outlet/>
+            </StyledStack>
+        </Stack>
+    );
+};
 
 export default Layout;
