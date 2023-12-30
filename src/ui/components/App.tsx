b9826e4b997d7d3df3fea9c2da8003c90639e880
Cameron Marr <alphaomegabeta@gmail.com>
2023-12-30 10:02:32 -0800

    Refactor: Move background style to theme

--- src/ui/components/App.tsx
+++ src/ui/components/App.tsx
@@ -9,11 +9,6 @@ import defaultTheme from "../themes/defaultTheme";
 import Router from "./Router";
 
 const StyledStack = styled(Stack)`
-    background: linear-gradient(100deg,
-    ${props => props.theme.palette.secondary.main}80,
-    ${props => props.theme.palette.secondary.main}25,
-    ${props => props.theme.palette.secondary.main}80),
-    ${props => props.theme.palette.background.default};
     min-width: 280px;
 `;
 
