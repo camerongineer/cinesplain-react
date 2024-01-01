555ad034b802719c3f92c99b2e61d7a2bfcc6ea4
Cameron Marr <alphaomegabeta@gmail.com>
2023-12-31 18:10:42 -0800

    Style: Modify padding, gaps, and size of icon

--- src/ui/components/content/homePage/Hero.tsx
+++ src/ui/components/content/homePage/Hero.tsx
@@ -11,33 +11,39 @@ const StyledStack = styled(Stack)`
     justify-content: center;
     align-items: center;
     text-align: center;
-    padding: 0;
+    padding: 10px;
+    gap: 1em;
     margin: 2em 0 1em;
     user-select: none;
     overflow-x: clip;
 `;
 
-const Hero: React.FC = () => (
-    <StyledStack direction="row">
-        <CSLoadingIcon
-            loadRotationMilliseconds={1250}
-            height={{ xs: 100, md: 125 }}
-        />
-        <Stack>
-            <Typography
-                component="h1"
-                variant="h2"
-            >
-                CineSplain
-            </Typography>
-            <Typography variant="h5">
-                Your home for all-things cinema
-            </Typography>
-            <Typography variant="subtitle2">
-                We've got some 'splaining to do...
-            </Typography>
-        </Stack>
-    </StyledStack>
-);
+const Hero: React.FC = () => {
+    
+    return (
+        <StyledStack direction="row">
+            <Stack>
+                <CSLoadingIcon
+                    loadRotationMilliseconds={1250}
+                    width={{ xs: 75, sm: 100, md: 125 }}
+                />
+            </Stack>
+            <Stack>
+                <Typography
+                    component="h1"
+                    variant="h3"
+                >
+                    CineSplain
+                </Typography>
+                <Typography variant="h6">
+                    Your home for all-things cinema
+                </Typography>
+                <Typography variant="subtitle2">
+                    We've got some 'splaining to do...
+                </Typography>
+            </Stack>
+        </StyledStack>
+    );
+};
 
 export default Hero;
