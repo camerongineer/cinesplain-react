9896ebf8a32b0a9a32ce7fb0c0e4944afe8695ef
Cameron Marr <alphaomegabeta@gmail.com>
2024-01-02 11:27:21 -0800

    Chore: Add initial PersonPage

---
+++ src/ui/components/content/personPage/ProfileCard.tsx
@@ -0,0 +1,39 @@
+import femaleSilhouette from "@assets/female_silhouette.png";
+import maleSilhouette from "@assets/male_silhouette.png";
+import {
+    Card,
+    CardMedia
+} from "@mui/material";
+import React from "react";
+import { PROFILE_SIZE } from "../../../../constants/ImageSizes.ts";
+import Person from "../../../../types/person.ts";
+import { getImagePath } from "../../../../utils/retrievalUtils.ts";
+
+interface ProfileCardProps {
+    person: Person;
+}
+
+const ProfileCard: React.FC<ProfileCardProps> = ({
+    person
+}) => {
+    const altSilhouette = person.gender === 1 ? femaleSilhouette : maleSilhouette;
+    return (
+        <Card sx={{
+            width: 280,
+            maxHeight: 632
+        }}>
+            <CardMedia
+                component="img"
+                loading="lazy"
+                image={
+                    person.profile_path
+                        ? getImagePath(person.profile_path, PROFILE_SIZE.LG_H632)
+                        : altSilhouette
+                }
+                alt={person.name}
+            />
+        </Card>
+    );
+};
+
+export default ProfileCard;
