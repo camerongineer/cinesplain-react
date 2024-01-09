4d52d66b9a5f0b22809c0002485332b86e615407
Cameron Marr <alphaomegabeta@gmail.com>
2024-01-09 13:10:18 -0800

    Chore: Delete unneeded files

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,10 @@
-import { DevSupport } from "@react-buddy/ide-toolbox";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./ui/styles/index.css";
-import {
-    ComponentPreviews,
-    useInitial
-} from "./dev";
 import App from "./ui/components/App.tsx";
 
 const root: ReactDOM.Root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    <DevSupport
-        ComponentPreviews={ComponentPreviews}
-        useInitialHook={useInitial}
-    >
         <StrictMode>
             <App/>
-        </StrictMode>
-    </DevSupport>);
+        </StrictMode>);
