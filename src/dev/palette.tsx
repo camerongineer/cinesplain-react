24de8288acf2fdc0333f3cc4b188ab97fabb82f8
Cameron Marr <alphaomegabeta@gmail.com>
2024-01-09 12:55:21 -0800

    Chore: Add react buddy previews

---
+++ src/dev/palette.tsx
@@ -0,0 +1,27 @@
+import {
+    Category,
+    Component,
+    Palette,
+    Variant
+} from "@react-buddy/ide-toolbox";
+import MUIPalette from "@react-buddy/palette-mui";
+import { Fragment } from "react";
+
+export const PaletteTree = () => (
+    <Palette>
+        <Category name="App">
+            <Component name="Loader">
+                <Variant>
+                    <ExampleLoaderComponent/>
+                </Variant>
+            </Component>
+        </Category>
+        <MUIPalette/>
+    </Palette>
+);
+
+export function ExampleLoaderComponent () {
+    return (
+        <Fragment>Loading...</Fragment>
+    );
+}
