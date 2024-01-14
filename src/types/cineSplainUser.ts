f30f0b9ad1cf7e628e62c48c395de0fd593bc81a
Cameron Marr <alphaomegabeta@gmail.com>
2024-01-14 14:22:02 -0800

    Chore: Add CineSplainUser type

---
+++ src/types/cineSplainUser.ts
@@ -0,0 +1,10 @@
+type CineSplainUser = {
+    id: string,
+    userName: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    method: string
+}
+
+export default CineSplainUser;
