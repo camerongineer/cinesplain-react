8708b24c8e4f3ec80c7a7e98d5606c044a929de5
Cameron Marr <alphaomegabeta@gmail.com>
2024-01-13 16:12:08 -0800

    Feat: Add Rotten Tomatoes Score row to MovieSideBar

---
+++ src/ui/components/content/common/RottenTomatoesDisplay.tsx
@@ -0,0 +1,36 @@
+import RottenTomatoes from "@assets/rottentomatoes_logo.svg?react";
+import {
+    Stack,
+    Typography
+} from "@mui/material";
+import React from "react";
+
+interface RottenTomatoesDisplayProps {
+    rottenTomatoesScore: number;
+}
+
+const RottenTomatoesDisplay: React.FC<RottenTomatoesDisplayProps> = ({
+    rottenTomatoesScore
+}) => (
+    <Stack
+        direction="row"
+        alignItems="center"
+        spacing={.5}
+    >
+        <RottenTomatoes width={20}/>
+        <Typography
+            display="inline"
+            fontWeight="bold"
+        >
+            Score:
+        </Typography>
+        <Typography
+            display="inline"
+            variant="h6"
+        >
+            {rottenTomatoesScore}
+        </Typography>
+    </Stack>
+);
+
+export default RottenTomatoesDisplay;
