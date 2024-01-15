19aca1256af6ec902f44214d9207499929652e02
Cameron Marr <alphaomegabeta@gmail.com>
2024-01-15 08:21:38 -0800

    Refactor: Extract CreditsListCastRow

---
+++ src/ui/components/content/personPage/CreditsListCastRow.tsx
@@ -0,0 +1,82 @@
+import {
+    Box,
+    Stack,
+    TableCell,
+    TableRow,
+    Typography
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { getImagePath } from "../../../../api/moviesApi.ts";
+import { POSTER_SIZE } from "../../../../constants/ImageSizes.ts";
+import CastMember from "../../../../types/castMember.ts";
+import Movie from "../../../../types/movie.ts";
+import { getFormattedMovieLinkId } from "../../../../utils/formatUtils.ts";
+
+const CreditsListCastRow = ({
+    castMemberCredit
+}: {
+    castMemberCredit: CastMember
+}) => {
+    
+    return (
+        <TableRow
+            component={Link}
+            to={`/movies/${getFormattedMovieLinkId(
+                {
+                    title: castMemberCredit.originalTitle,
+                    id: castMemberCredit.id
+                } as unknown as Movie)}`
+            }
+            sx={{
+                "&:hover": {
+                    backgroundColor: "#f0f0f0"
+                },
+                "&:last-child td, &:last-child th": { border: 0 }
+            }}
+        >
+            <TableCell
+                component="th"
+                scope="row"
+            >
+                <Typography
+                    variant="subtitle2"
+                    fontWeight="bold"
+                >
+                    {castMemberCredit.releaseDate
+                        ? new Date(castMemberCredit.releaseDate).getFullYear()
+                        : "Upcoming"}
+                </Typography>
+            </TableCell>
+            <TableCell align="right">
+                <Stack
+                    direction="row"
+                    justifyContent="end"
+                    alignItems="center"
+                    spacing={2}
+                >
+                    <Stack>
+                        <Typography
+                            variant="subtitle2"
+                            fontWeight="bold"
+                        >
+                            {castMemberCredit.title}
+                        </Typography>
+                        {castMemberCredit.character && <Typography
+                            variant="subtitle2"
+                            fontSize="small"
+                        >
+                            as {castMemberCredit.character}
+                        </Typography>}
+                    </Stack>
+                    {castMemberCredit.posterPath && <Box
+                        component="img"
+                        src={getImagePath(castMemberCredit.posterPath, POSTER_SIZE.XXXS_W92)}
+                        width={45}
+                    />}
+                </Stack>
+            </TableCell>
+        </TableRow>
+    );
+};
+
+export default CreditsListCastRow;
