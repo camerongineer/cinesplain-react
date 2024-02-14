48018452ed288c0827eb15c8f44756de6b383888
Cameron Marr <alphaomegabeta@gmail.com>
2024-02-13 18:09:28 -0800

    fix: modify getCSRatingColor to reflect current rating scale

--- src/utils/ratingUtils.ts
+++ src/utils/ratingUtils.ts
@@ -1,3 +1,3 @@
 export const getCSRatingColor = (rating: number) => {
-    return rating < 60 ? "rgba(236,122,39,0.7)" : rating < 80 ? "rgba(255,132,232,0.7)" : "rgba(76,67,212,0.7)";
+    return rating < 6.0 ? "rgba(236,122,39,0.7)" : rating < 8.0 ? "rgba(255,132,232,0.7)" : "rgba(76,67,212,0.7)";
 };
