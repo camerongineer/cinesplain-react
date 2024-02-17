4566e18aebb2ee97c7bd931b45091b118787f56c
Cameron Marr <alphaomegabeta@gmail.com>
2024-02-17 11:37:56 -0800

    chore: add listDisplayMovieCrewCredit type

---
+++ src/types/listDisplayMovieCrewCredit.ts
@@ -0,0 +1,15 @@
+type ListDisplayMovieCrewCredit = {
+    adult: boolean;
+    creditId: string;
+    department: string;
+    gender: number;
+    id: number;
+    knownForDepartment: string;
+    job: string;
+    name: string;
+    originalName: string;
+    popularity: number;
+    profilePath: string | null;
+};
+
+export default ListDisplayMovieCrewCredit;
