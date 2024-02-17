41413e68f19f08343f67d14642866ce57a9c254c
Cameron Marr <alphaomegabeta@gmail.com>
2024-02-17 11:37:50 -0800

    chore: add listDisplayMovieCastCredit type

---
+++ src/types/listDisplayMovieCastCredit.ts
@@ -0,0 +1,15 @@
+type ListDisplayMovieCastCredit = {
+    adult: boolean;
+    creditId: string;
+    character: string;
+    gender: number;
+    id: number;
+    knownForDepartment: string;
+    order: number;
+    name: string;
+    originalName: string;
+    popularity: number;
+    profilePath: string | null;
+};
+
+export default ListDisplayMovieCastCredit;
